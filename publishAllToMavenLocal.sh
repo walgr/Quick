b0d52fbbb1b46a1ebf949b8b7d8cff3094849dc9
王朋飞 <wpf6457@qq.com>
2022-07-15 16:09:54 +0800

    日常提交 2022-07-15.

--- publishAllToMavenLocal.sh
+++ publishAllToMavenLocal.sh
@@ -1,4 +1,6 @@
 ./gradlew :Quick-annotations:publishToMavenLocal
 ./gradlew :Quick-runtime:publishToMavenLocal
+./gradlew :Quick-gradle-plugin:publishToMavenLocal
+./gradlew :Quick-compiler:publishToMavenLocal
 ./gradlew :QuickBind:publishToMavenLocal
 ./gradlew :Quick:publishToMavenLocal
