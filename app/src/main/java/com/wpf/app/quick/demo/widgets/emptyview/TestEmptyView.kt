21337685d6f6222ee17b7355f40da0a0e6d2cc24
王朋飞 <walgr1010>
2022-11-11 17:57:09 +0800

    日常提交:2022-11-11.
    Signed-off-by: 王朋飞 <walgr1010>

--- app/src/main/java/com/wpf/app/quick/demo/widgets/emptyview/TestEmptyView.kt
+++ app/src/main/java/com/wpf/app/quick/demo/widgets/emptyview/TestEmptyView.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import com.wpf.app.quick.demo.R
-import com.wpf.app.quickutil.bind.Bind
-import com.wpf.app.quickutil.bind.QuickBindWrap
 import com.wpf.app.quickutil.widgets.emptyview.*
 
 open class TestEmptyView(
