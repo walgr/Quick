6600a279645abfa3f73aa08334b79197f5f2e058
王朋飞 <walgr1010>
2023-07-07 17:53:56 +0800

    日常修改：2023-07-07.

--- Quick-gradle-kotlin-plugin/src/main/java/com/wpf/plugins/activity/GetClass.kt
+++ Quick-gradle-kotlin-plugin/src/main/java/com/wpf/plugins/activity/GetClass.kt
@@ -1,19 +1,27 @@
 package com.wpf.plugins.activity
 
+import com.google.devtools.ksp.getDeclaredFunctions
 import com.google.devtools.ksp.processing.Dependencies
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
+import com.google.devtools.ksp.symbol.KSAnnotation
 import com.google.devtools.ksp.symbol.KSClassDeclaration
 import com.google.devtools.ksp.symbol.KSVisitorVoid
+import com.squareup.kotlinpoet.ANNOTATION
 import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.TypeSpec
+import com.squareup.kotlinpoet.asClassName
+import kotlin.reflect.KClass
 
 @Target(AnnotationTarget.CLASS, AnnotationTarget.ANNOTATION_CLASS)
 @Retention(AnnotationRetention.SOURCE)
 annotation class GetClass
 
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.SOURCE)
+annotation class GetFun
 
 internal class GetClassProcessor(private val environment: SymbolProcessorEnvironment) :
     KSVisitorVoid() {
@@ -25,13 +33,16 @@ internal class GetClassProcessor(private val environment: SymbolProcessorEnviron
         if (packageName.isNullOrEmpty()) return
         val classAddName = "Class"
         val fileName = "$className$classAddName"
+        classDeclaration.getDeclaredFunctions().map {
+            environment.logger.warn("方法:${it.simpleName}")
+        }
         val fileSpec = FileSpec.builder(packageName, fileName)
             .addType(
                 TypeSpec.objectBuilder(fileName)
                     .addFunction(
                         FunSpec.builder("getJClass")
                             .returns(
-                                ClassName("java.lang", "Class")
+                                Class::class.asClassName()
                                     .parameterizedBy(ClassName(packageName, className))
                             )
                             .addStatement(
@@ -42,7 +53,7 @@ internal class GetClassProcessor(private val environment: SymbolProcessorEnviron
                     .addFunction(
                         FunSpec.builder("getKClass")
                             .returns(
-                                ClassName("kotlin.reflect", "KClass")
+                                KClass::class.asClassName()
                                     .parameterizedBy(ClassName(packageName, className))
                             )
                             .addStatement(
@@ -50,6 +61,16 @@ internal class GetClassProcessor(private val environment: SymbolProcessorEnviron
                             )
                             .build()
                     )
+                    .addFunctions(classDeclaration.getDeclaredFunctions().filter {
+                        it.annotations.map {
+                            it.shortName.asString()
+                        }.contains("GetFun")
+                    }.map {
+                        FunSpec.builder(it.simpleName.getShortName())
+                            .returns(String::class)
+                            .addStatement("return %S", it.simpleName.getShortName())
+                            .build()
+                    }.asIterable())
                     .build()
             )
             .build()
--- app/src/main/java/com/wpf/app/quick/demo/http/TestApi.kt
+++ app/src/main/java/com/wpf/app/quick/demo/http/TestApi.kt
@@ -3,14 +3,15 @@ package com.wpf.app.quick.demo.http
 import com.wpf.app.quick.demo.http.call.TestCommonCall
 import com.wpf.app.quick.demo.wanandroid.model.首页文章
 import com.wpf.plugins.activity.GetClass
+import com.wpf.plugins.activity.GetFun
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
-import kotlin.reflect.KClass
 
 @GetClass
 interface TestApi {
 
+    @GetFun
     @GET("/article/list/{page}/json")
     fun 首页文章列表(
         @Path("page") page: Int,
