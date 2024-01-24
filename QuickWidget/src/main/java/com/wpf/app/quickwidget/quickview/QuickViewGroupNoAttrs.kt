bdf686a5e5944513ddac4ee373b652a539c7077a
王朋飞 <wpf6457@qq.com>
2024-01-24 14:59:37 +0800

    日常修改：2024-01-24.

--- QuickWidget/src/main/java/com/wpf/app/quickwidget/quickview/QuickViewGroupNoAttrs.kt
+++ QuickWidget/src/main/java/com/wpf/app/quickwidget/quickview/QuickViewGroupNoAttrs.kt
@@ -1,12 +1,10 @@
 package com.wpf.app.quickwidget.quickview
 
 import android.content.Context
-import android.graphics.Canvas
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.widget.FrameLayout
 import android.widget.LinearLayout
@@ -14,14 +12,9 @@ import android.widget.RadioGroup
 import android.widget.RelativeLayout
 import androidx.annotation.CallSuper
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.isInvisible
-import androidx.core.view.isVisible
 import com.google.android.material.tabs.TabLayout
-import com.wpf.app.quickutil.helper.attribute.AutoGetAttribute
 import com.wpf.app.quickutil.helper.attribute.AutoGetAttributeHelper
-import com.wpf.app.quickutil.other.GenericEx
 import com.wpf.app.quickutil.other.asTo
-import com.wpf.app.quickutil.view.matchLayoutParams
 import com.wpf.app.quickwidget.R
 
 /**
@@ -55,7 +48,6 @@ open class QuickViewGroupNoAttrs<T : ViewGroup> @JvmOverloads constructor(
         if (isInEditMode) {
             addToParent = false
         }
-        setWillNotDraw(false)
         shadowView = initViewGroupByXml(shadowView, attrSet.groupType, context, attrs, defStyleAttr)
             ?: initViewGroupByT(shadowView, context, attrs, defStyleAttr)
         addChildToT(shadowView, this)
