c7aa92f5be5d6738d74af098650c6d0341fa5142
王朋飞 <wpf6457@qq.com>
2024-04-19 09:53:43 +0800

    日常修改：2024-04-19.

--- app/src/main/java/com/wpf/app/quick/demo/fragment/MainTestFragment.kt
+++ app/src/main/java/com/wpf/app/quick/demo/fragment/MainTestFragment.kt
@@ -1,9 +1,9 @@
 package com.wpf.app.quick.demo.fragment
 
+import com.wpf.app.base.ability.base.with
 import com.wpf.app.quick.ability.QuickFragment
 import com.wpf.app.quick.ability.ex.binding
 import com.wpf.app.quick.ability.ex.contentView
-import com.wpf.app.base.ability.base.with
 import com.wpf.app.quick.demo.R
 import com.wpf.app.quick.demo.databinding.FragmentMainTestBinding
 import com.wpf.app.quickutil.helper.dp
@@ -14,9 +14,9 @@ class MainTestFragment : QuickFragment(
     contentView(R.layout.fragment_main_test)
         .with(binding<FragmentMainTestBinding> {
             llRoot.postDelay(2000) {
-                llRoot.setNewItemSpaceWithAnim(20.dp())
-                ll1.setNewItemSpaceWithAnim(20.dp())
-                ll2.setNewItemSpaceWithAnim(20.dp())
+                llRoot.setNewItemSpaceWithAnim(20.dp)
+                ll1.setNewItemSpaceWithAnim(20.dp)
+                ll2.setNewItemSpaceWithAnim(20.dp)
             }
             seekbar.onProgressChanged { _, progress, _ ->
                 llRoot.setNewItemSpace(progress)
